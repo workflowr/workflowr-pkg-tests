0ccf2a90ac70412a04b7cebb8e3252c1bdbabc8b
John Blischak <jdblischak@gmail.com>
2019-10-17 16:42:53 -0400

    Add more commits to repo for speed test.

--- scripts/speed-test.R
+++ scripts/speed-test.R
@@ -7,5 +7,9 @@ path <- fs::file_temp()
 wflow_start(path, change_wd = FALSE, user.name = "Test",
             user.email = "test@test.com")
 rmd <- fs::dir_ls(fs::path(path, "analysis"), glob = "*Rmd")
-lapply(rmd, function(x) cat("\nedit\n", file = x, append = TRUE))
-system.time(wflow_publish(rmd, "commit message", view = FALSE, project = path))
+for (i in 1:100) {
+  tmp <- lapply(rmd, function(x) cat("\nedit\n", file = x, append = TRUE))
+  wflow_git_commit(rmd, project = path)
+}
+tmp <- lapply(rmd, function(x) cat("\nedit\n", file = x, append = TRUE))
+system.time(publish <- wflow_publish(rmd, "commit message", view = FALSE, project = path))
