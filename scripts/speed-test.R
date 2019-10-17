563b64523a88a86ecdf04c30c4cb4bf159335cf8
John Blischak <jdblischak@gmail.com>
2019-10-17 12:26:18 -0400

    Configure local Git config for speed test.

--- scripts/speed-test.R
+++ scripts/speed-test.R
@@ -4,7 +4,8 @@
 
 library(workflowr)
 path <- fs::file_temp()
-wflow_start(path, change_wd = FALSE)
+wflow_start(path, change_wd = FALSE, user.name = "Test",
+            user.email = "test@test.com")
 rmd <- fs::dir_ls(fs::path(path, "analysis"), glob = "*Rmd")
 lapply(rmd, function(x) cat("\nedit\n", file = x, append = TRUE))
 system.time(wflow_publish(rmd, "commit message", project = path))
