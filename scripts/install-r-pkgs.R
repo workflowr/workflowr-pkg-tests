862c7aa1366ebb596c5314c5206e43e047f74ebb
John Blischak <jdblischak@gmail.com>
2019-10-17 14:00:23 -0400

    Install current version of remotes since it may not be available from MRAN snapshot.

--- scripts/install-r-pkgs.R
+++ scripts/install-r-pkgs.R
@@ -2,7 +2,7 @@
 
 cat("Installing remotes\n")
 if (!requireNamespace("remotes", quietly = TRUE))
-  install.packages("remotes")
+  install.packages("remotes", repos = "https://cran.rstudio.com/")
 
 cat("Installing outdated dependencies\n")
 remotes::install_deps(pkgdir = "/tmp/workflowr", dependencies = TRUE)
