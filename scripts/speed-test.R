13b69e330c5adeb71bd96a275f0675221424c8dc
John Blischak <jdblischak@gmail.com>
2019-10-17 13:36:14 -0400

    Use Rscript to run speed test.
    For some reason, littler fails because no files are created in docs/, even though wflow_build completes successfully. Adding the -t flag didn't help.

--- scripts/speed-test.R
+++ scripts/speed-test.R
@@ -8,4 +8,4 @@ wflow_start(path, change_wd = FALSE, user.name = "Test",
             user.email = "test@test.com")
 rmd <- fs::dir_ls(fs::path(path, "analysis"), glob = "*Rmd")
 lapply(rmd, function(x) cat("\nedit\n", file = x, append = TRUE))
-system.time(wflow_publish(rmd, "commit message", project = path))
+system.time(wflow_publish(rmd, "commit message", view = FALSE, project = path))
