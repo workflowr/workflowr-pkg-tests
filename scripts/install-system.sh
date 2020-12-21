fbd195dc560cec635980db0081c529fe5b02ec98
John Blischak <jdblischak@gmail.com>
2020-12-21 11:58:47 -0500

    Use stretch-backports to install recent libgit2 version required by gert.

--- scripts/install-system.sh
+++ scripts/install-system.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -eux
 
-# Use PPA to install libgit2 for gert, a dependency of usethis/devtools
-# https://github.com/r-lib/gert/issues/107#issuecomment-744465884
-apt-get update
-apt-get install -y \
-  software-properties-common
-add-apt-repository --yes ppa:cran/libgit2
+# Use stretch-backports to install recent libgit2 version required by
+# gert
+# https://github.com/r-lib/gert/issues/112
+# https://packages.debian.org/stretch-backports/libgit2-dev
+# https://linuxconfig.org/how-to-install-and-use-debian-backports
+debian=`lsb_release --codename | cut -f2`
+if [ $debian == 'stretch' ]
+then
+  echo 'Using stretch-backports to install libgit2-dev'
+  echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list
+  apt update
+  apt install -y -t stretch-backports libgit2-dev
+fi
 
 apt-get update
 apt-get install -y \
